94e5bb08c945ab3d537698f1004738ff7b702ae5
ldowns99 <42652512+ldowns99@users.noreply.github.com>
2018-08-23 12:47:34 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Luke Downs
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Luke, Computer Science\n");
+
+  return 0;
+}
